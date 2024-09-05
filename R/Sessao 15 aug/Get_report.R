351f6c50cae6b9b4cce83a0c041b7445dadcf873
araupontones <43657130+araupontones@users.noreply.github.com>
2024-09-05 11:57:59 +0200

    figure out how to get more than 1000 recs

--- R/Sessao 15 aug/Get_report.R
+++ R/Sessao 15 aug/Get_report.R
@@ -1,15 +1,16 @@
 #Exercise to GET data from one record
-library(jsonlite)
+
 #load secrets and server info
 gmdacr::load_functions('vault/')
+gmdacr::load_functions('utils/')
 
 
 #1. Define report parameters ===================================================
 
 
 account_owner_name <- 'araupontones'
-app_link_name <- 'realiza'
-report_link_name <- 'Presencas_IND_SGR_report'
+app_link_name <- 'uk-pact'
+report_link_name <- 'Download_outputs'
 
 
 
@@ -20,13 +21,15 @@ access_token <- create_access_token()
 
 
 
-request_url_get <- glue('https://{base_url}/creator/v2.1/data/{account_owner_name}/{app_link_name}/report/{report_link_name}')
+request_url_get <- glue('https://{server_info()$base_url}/creator/v2.1/data/{account_owner_name}/{app_link_name}/report/{report_link_name}')
 
+#first request =================================================
 request_get <- request(request_url_get) |>
   req_headers(
     Authorization = paste('Zoho-oauthtoken', access_token),
     accept = 'application/json'
-  )
+  ) |>
+  req_url_query(max_records = 1000)
 
 
 
@@ -36,38 +39,64 @@ req_dry_run(request_get)
 resp_get <- request_get |> req_perform()
 
 status <- resp_status(resp_get)
+status
+#get data of response
+resp_json_reporte <- resp_get |> resp_body_json()
 
-#if token expiro, create a new 
-if(status != 200){
-  
-  req <- request(request_url_refresh) |>
-    req_method('POST')
-  
-  #Check the request
-  req_dry_run(req)
-  
-  # get access token : remember that expires every 3,600 segundos
-  
-  resp <- req |> req_perform()
-  
-  resp_json <- resp_body_json(resp)
-  access_token <- resp_json$access_token
-  
-  #Perform the request again
-  resp_get <- request_get |> req_perform()
-  resp_json_reporte <- resp_get |> resp_body_json()
-  
-  
-  
-}
+#check length or records
+length(resp_json_reporte$data)
 
-if(status == 200){
-  
-  resp_json_reporte <- resp_get |> resp_body_json()
-  
-  
-}
+#get the record of the last observation so we can request it
+
+resp_headers(resp_get)
+ultimo_record <- resp_headers(resp_get)$record_cursor
+
+#request the second batch
+request_get <- request(request_url_get) |>
+  req_headers(
+    Authorization = paste('Zoho-oauthtoken', access_token),
+    record_cursor = ultimo_record,
+    accept = 'application/json'
+  )
+
+
+resp_get2 <- request_get |> req_perform()
+
+status2 <- resp_status(resp_get)
+status2
+resp_json_reporte2 <- resp_get2 |> resp_body_json()
 
 
 
 
+
+my_report <- resp_json_reporte$data %>% do.call(rbind,.)
+
+nrow(my_report)
+
+resp_json_reporte$data[1]
+resp_body_string(resp_get)
+#if token expiro, create a new ===============================================
+# if(status != 200){
+#   
+#   
+#   access_token <- create_access_token()
+#   
+#   #Perform the request again
+#   resp_get <- request_get |> req_perform()
+#   resp_json_reporte <- resp_get |> resp_body_json()
+#   
+#   
+#   
+# }
+# 
+# if(status == 200){
+#   
+#   resp_json_reporte <- resp_get |> resp_body_json()
+#   
+#   
+# }
+# 
+# 
+# 
+# 
